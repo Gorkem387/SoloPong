b3e6b8fde48d398cd5d4a27bccfed2587b1c478a
Gorkem387 <gorkemyildiz7ds@gmail.com>
2025-09-19 12:57:09 +0200

    initialisation de la balle

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ let rect = {
     height: 10
 }
 
+let ball = {
+    x: (canvas.width - rect.width) / 2 + 40,
+    y: canvas.height - rect.height - 13,
+    radius: 8,
+    dx: Math.random() < 0.5 ? 3 : -3,
+    dy: 3,
+    speed: 4
+}
+
 rect.x = (canvas.width - rect.width) / 2;
 rect.y = canvas.height - rect.height - 5;
 
@@ -22,16 +31,41 @@ function drawRect() {
     ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
 }
 
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function updateBallPosition() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
 drawRect();
+drawBall();
+
+function gameLoop() {
+    if (startGame) {
+        drawRect();
+        drawBall();
+        updateBallPosition();
+        requestAnimationFrame(gameLoop);
+    }
+}
 
 window.addEventListener("keydown", function(e) {
                 switch (e.code) {
                     case 'ArrowLeft':
+                    case 'KeyA' :
                         if(rect.x - 5 >= 0) {
                             rect.x -= 5;
                         }
                         break;
                     case 'ArrowRight':
+                    case 'KeyD' :
                         if(rect.x + rect.width + 5 <= canvas.width) {
                             rect.x += 5;
                         }
@@ -41,10 +75,15 @@ window.addEventListener("keydown", function(e) {
                 drawRect();
         })
 
-window.addEventListener('keydown', startingGame);
+window.addEventListener("keydown", startingGame);
 
 function startingGame(e) {
-    if (e.code) {
+    if (!startGame && 
+        (e.code === "ArrowLeft" || 
+         e.code === "ArrowRight" || 
+         e.code === "KeyA" || 
+         e.code === "KeyD")) {
         startGame = true;
+        gameLoop();
     }
 }
